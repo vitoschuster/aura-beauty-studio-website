aa1b43d64325418316fce22a2935ad293b6cd8c0
Vito Schuster <vitoschuster@Vitos-MacBook-Air-2.local>
2026-04-01 19:42:17 +0200

    Fix static deployment config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: 'https://aurabeautystudio.com.hr',
   output: 'static',
-  adapter: cloudflare(),
 });
